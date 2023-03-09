180b755fb614570cbc4908e26171a15c357bab15
ayo-kunle <edwardsayokunle@gmail.com>
2023-03-09 18:39:46 +0100

    first commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -66,18 +66,18 @@ const Welcome = () => {
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
-              Reliability
+              Dependability
             </div>
-            <div className={companyCommonStyles}>Security</div>
+            <div className={companyCommonStyles}>Encryption</div>
             <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
-              Ethereum
+              Sccmart Contract Platform
             </div>
             <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
-              Web 3.0
+              Blockchain Web
             </div>
-            <div className={companyCommonStyles}>Low Fees</div>
+            <div className={companyCommonStyles}>Affordability</div>
             <div className={`rounded-br-2xl ${companyCommonStyles}`}>
-              Blockchain
+              Decentralized database
             </div>
           </div>
         </div>
